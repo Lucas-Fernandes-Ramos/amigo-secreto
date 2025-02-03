333002ea4bb9310eb480fd791d7dbeef3b573fb6
Lucas Fernandes <lucas-fernandes87@hotmail.com>
2025-02-02 22:50:19 -0300

    finalizando a função sortearAmigo

--- app.js
+++ app.js
@@ -27,19 +27,31 @@ function adicionarAmigo(){
             console.log(listaElementosUnicos);//para teste
     }
 
-    function limparCampo() {
-        //limpando o campo input
-        campo = document.querySelector('input');
-        campo.value = '';
-    }
+   
 
+}//fim da função adicionar
 
-     function mostrarLista(){
-       //transformando cada elemento do array em um li html
-       document.getElementById('listaAmigos').innerHTML = 
-       novaLista.map(item=>`<li>${item}</li>`).join('');
-
-     }
- 
-
+function limparCampo() {
+    //limpando o campo input
+    campo = document.querySelector('input');
+    campo.value = '';
 }
+
+function mostrarLista(){
+    //transformando cada elemento do array em um li html
+    document.getElementById('listaAmigos').innerHTML = 
+    novaLista.map(item=>`<li>${item}</li>`).join('');
+
+  }
+
+function sortearAmigo(){
+    //verificar se a lista não esta vazia
+    if(novaLista.length < 2){
+        alert("Para realizarmos o sorteio, será necessario 2 ou mais amigos");
+    }else{
+        const elemento = novaLista[Math. floor(Math. random() * novaLista. length)]
+        console.log(`o amigo sorteado é ${elemento}!`)
+        //mostrando o ganhador na tela
+        document.getElementById('resultado').innerHTML = elemento;
+    }
+ }
